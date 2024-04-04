f00e699fe1a97e382c74d6b151842d015a755bd8
Ceri Dorsey-Tyler <cerimay@gmail.com>
2024-04-04 16:59:31 -0400

    test2

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+// Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <section className="py-16 bg-gray-100">
+      <div className="container mx-auto text-center">
+        <h2 className="text-3xl font-bold mb-4">Welcome to Have Fun Tattoo</h2>
+        <p className="text-lg">Express yourself with our talented artists.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
