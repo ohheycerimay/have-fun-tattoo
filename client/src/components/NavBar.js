594c7f4ea8617d4b9b01bdd3c6e839cad44b1a49
Ceri Dorsey-Tyler <cerimay@gmail.com>
2024-04-04 17:40:58 -0400

    aftercare

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,13 +4,14 @@ import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <nav className="bg-cyan-400 text-white font-custom py-4">
+    <nav className="bg-cyan-400 text-white font-custom py-4 text-center">
       <div className="container mx-auto">
-        <ul className="flex space-x-4">
+        <ul className="text-center">
           <li><NavLink to="/" className="hover:text-gray-300">Home</NavLink></li>
           <li><NavLink to="/about" className="hover:text-gray-300">About</NavLink></li>
           <li><NavLink to="/artists" className="hover:text-gray-300">Artists</NavLink></li>
           <li><NavLink to="/booking" className="hover:text-gray-300">Booking Policies</NavLink></li>
+          <li><NavLink to="/aftercare" className="hover:text-gray-300">Aftercare</NavLink></li>
         </ul>
       </div>
     </nav>
